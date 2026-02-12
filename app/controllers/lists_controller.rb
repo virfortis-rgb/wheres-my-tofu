552a823a38bb1051b82cad1e6376ab77d0bbe675
Mahmud Md Fuad <102977353+fuadaman@users.noreply.github.com>
2026-02-12 21:45:00 +0900

    Added lists and items controller.

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,18 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+  def show
+  end
+
+  def destroy
+
+  end
+end
