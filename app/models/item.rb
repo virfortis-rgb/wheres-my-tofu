1a952972f32ad3d24416d5ab13ddb939724e717d
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-18 17:24:07 +0900

    add find_product method to Item Model

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,10 @@ class Item < ApplicationRecord
   belongs_to :price, optional: true
   # add this line to allow to access price on show page through item.price
 
-
-
   validates :name, presence: :true
-  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+
+  def find_products
+    products = Product.where(name: self.name)
+  end
 end
