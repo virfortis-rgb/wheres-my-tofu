183a409b3830c66317b39c5f472bb75cd6655a4d
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-23 11:50:07 +0900

    add shufoo url and shop ids for Ainan

--- app/jobs/retrieve_product_info_from_chirashi_job.rb
+++ app/jobs/retrieve_product_info_from_chirashi_job.rb
@@ -2,7 +2,19 @@ class RetrieveProductInfoFromChirashiJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts "Starting chirashi retrieval for place."
+    puts "Starting chirashi retrieval for #place."
+    # scrape shufoo for newest and most relavant chirashis for given area, using OpenURI ?
+      # need shop ids
+    shop_ids = {
+      "フジ南宇和店": 267098,
+      "エースワン": 258215
+    }
+    shufoo_base_url = "https://www.shufoo.net/pntweb/shopDetail/#{shop_id}/"
+
+
+    # feed those chirashis to ruby_llm to parse into JSON readable by the DB, sleep between each chirashi
+    # Product.find_or_create_by! as well as Store.find_or_create_by!
+    # update price
 
     puts "Product information in #place area updated from chirashi."
   end
