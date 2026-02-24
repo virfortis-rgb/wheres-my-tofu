76975ac0479ff8f4a413ac10dc9a69d3b2f2c04c
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-24 21:11:52 +0900

    add cloudinary gem

--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,7 +1,14 @@
 class ScansController < ApplicationController
   def new
+    @scan = Scan.new
   end
 
   def create
+  end
+
+  private
+
+  def scan_params
+    params.require(:scan).permit
   end
 end
