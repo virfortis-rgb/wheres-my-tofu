555e76290e44dee6981f82461aaecb5ac9362f9f
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-26 18:28:36 +0900

    add solid_queue gem

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module WheresMyTofu
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_job.queue_adapter = :solid_queue
   end
 end
