83ba511a436b518ba036a0b0d0d27ceef5d655e8
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-26 18:32:31 +0900

    config solid_queue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :solid_queue
-  config.solid_queue.connects_to = { database: { writing: :queue } }
   # config.active_job.queue_name_prefix = "wheres_my_tofu_production"
 
   config.action_mailer.perform_caching = false
