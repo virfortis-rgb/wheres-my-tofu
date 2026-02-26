412cf57591c3d07949b415c0a685b26bc438d48e
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-26 19:38:46 +0900

    config ruby_llm

--- app/jobs/add_flyer_data_to_db_job.rb
+++ app/jobs/add_flyer_data_to_db_job.rb
@@ -1,10 +1,14 @@
 class AddFlyerDataToDbJob < ApplicationJob
   queue_as :default
 
+  SYSTEM_PROMPT = "Generate a json in the following format from this image of a flyer: "
+
   def perform(scan)
     # set the llm
+    chat = RubyLLM.chat(model: 'gpt-4o')
     # give scan to llm
-    # get json back
+    response = chat.ask SYSTEM_PROMPT, with: { image: scan}
     # add to DB
+
   end
 end
