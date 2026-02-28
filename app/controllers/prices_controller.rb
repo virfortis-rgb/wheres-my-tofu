d673729662fdc7fa650b7bfae8cedb8041487ecc
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-28 18:10:47 +0900

    add prices/edit partial

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,7 +1,12 @@
 class PricesController < ApplicationController
 
   def update
-
+    @price = Price.find(params[:id])
+    if @price.update(price_params)
+      redirect_to scan_path(params[:scan_id]), notice: "Updated!"
+    else
+      redirect_to scan_path(params[:scan_id]), alert: "Failed to update!"
+    end
   end
 
   def destroy
@@ -10,5 +15,6 @@ class PricesController < ApplicationController
   private
 
   def price_params
+    params.require(:price).permit(:price_with_tax, :price_without_tax, :scan_id, product_attributes: [:id, :name, :description])
   end
 end
