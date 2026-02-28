dec475136ebf5e5442c107b34d9a8286e0891819
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-28 17:21:50 +0900

    add modal and form in scans#show

---
+++ app/controllers/prices_controller.rb
@@ -0,0 +1,14 @@
+class PricesController < ApplicationController
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+
+  def price_params
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   end
 
   resources :scans, only: [:new, :show, :create]
+  resources :prices, only: [:update, :destroy]
 end
