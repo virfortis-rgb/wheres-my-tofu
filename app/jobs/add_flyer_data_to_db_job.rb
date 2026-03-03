3ad90eae6d8b35bac6c9fe2c58ef13df9c5b3111
Damien Joubert <damienusvirfortis@gmail.com>
2026-03-03 19:39:31 +0900

    refactor job

--- app/jobs/add_flyer_data_to_db_job.rb
+++ app/jobs/add_flyer_data_to_db_job.rb
@@ -1,12 +1,8 @@
 class AddFlyerDataToDbJob < ApplicationJob
   queue_as :default
 
-  SYSTEM_PROMPT = "Extract data from this image/pdf and add it to the DB."
-
   def perform(scan)
-    if scan.flyer.attached?
-      process_scan(scan)
-    end
+      process_scan(scan) if scan.flyer.attached?
   end
 
     private
@@ -20,16 +16,14 @@ class AddFlyerDataToDbJob < ApplicationJob
       the store_longitude is #{store.longitude},
       the store_latitude is #{store.latitude},
       the store_id is #{store.id}
-      IMPORTANT: use store id for price_store_id param!
+      IMPORTANT: use the store id for price_store_id param!
       "
     if flyer.content_type == "application/pdf"
       chat = RubyLLM.chat(model: "gemini-2.0-flash").with_tool(FlyerReaderTool)
-      # chat.with_instructions(SYSTEM_PROMPT)
       chat.ask(prompt, with: { pdf: flyer.url})
       puts "processing scan ..."
     elsif flyer.image?
       chat = RubyLLM.chat(model: 'gpt-4o').with_tool(FlyerReaderTool)
-      # chat.with_instructions(SYSTEM_PROMPT)
       chat.ask(prompt, with: { image: flyer.url})
       puts "processing scan ..."
     end
