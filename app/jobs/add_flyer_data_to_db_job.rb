6a62567101105ae910034842e0c4511e56289c53
Damien Joubert <damienusvirfortis@gmail.com>
2026-03-07 19:49:31 +0900

    add initiliaze method to tool in order to pass the store instance through. In job, iterate over results and make scanprice instances

--- app/jobs/add_flyer_data_to_db_job.rb
+++ app/jobs/add_flyer_data_to_db_job.rb
@@ -11,28 +11,30 @@ class AddFlyerDataToDbJob < ApplicationJob
   def process_scan(scan)
     store = scan.store
     flyer = scan.flyer
-    prompt = "#{SYSTEM_PROMPT}, store contenxt for this flyer:
-      the store_name is #{store.name},
-      the store_address is #{store.address},
-      the store_longitude is #{store.longitude},
-      the store_latitude is #{store.latitude},
-      the store_id is #{store.id}
-      IMPORTANT: use the store id for store_id param!
-      "
+    tool = FlyerReaderTool.new(store)
+    #   prompt = " store contenxt for this flyer: the store_name is #{store.name},
+    #   the store_address is #{store.address},
+    #   the store_longitude is #{store.longitude},
+    #   the store_latitude is #{store.latitude},
+    #   # the store_id is #{store.id}
+    #   # IMPORTANT: use the store id for store_id param!"
     if flyer.content_type == "application/pdf"
-      chat = RubyLLM.chat(model: "gemini-2.0-flash").with_tool(FlyerReaderTool)
-      chat.ask(prompt, with: { pdf: flyer.url })
+      chat = RubyLLM.chat(model: "gemini-2.0-flash").with_tool(tool)
+      chat.ask(SYSTEM_PROMPT, with: { pdf: flyer.url })
       puts "processing scan ..."
     elsif flyer.image?
       chat = RubyLLM.chat(model: 'gpt-4.1-nano', provider: :openai, assume_model_exists: true)
                     .with_schema(FlyerSchema)
-                    .with_tool(FlyerReaderTool)
+                    .with_tool(tool)
                     .on_tool_result do |result|
                       # create instances of scan_price out of result (array of prices)
-                      # add store.id
-                      puts "Tool call: #{result}"
+                      pp result
+                      result.each do |price|
+                        ScanPrice.create(price: price, scan: scan)
+                        puts "🎴ScanPrice instance created ..."
+                      end
                     end
-      chat.ask(prompt, with: { image: flyer.url })
+      chat.ask(SYSTEM_PROMPT, with: { image: flyer.url })
       #
       puts "processing scan ..."
     end
--- app/tools/flyer_reader_tool.rb
+++ app/tools/flyer_reader_tool.rb
@@ -1,14 +1,18 @@
 class FlyerReaderTool < RubyLLM::Tool
   description "Use this tool to extract products and prices from a flyer and add create an array of prices.
               Keep the product name and description in Japanese."
+  def initialize(store)
+    @store = store
+  end
 
   params do
     object :scanned_products do
       string :name, description: "商品名"
       string :description, description: "商品説明文"
       string :keyword, description: "One keyword to define this product"
-      string :price_without_tax, description: "The price without tax"
-      string :price_with_tax, description: "The price with tax (税込)"
+      number :price_without_tax, description: "The price without tax"
+      number :price_with_tax, description: "The price with tax (税込)"
+      # string :store_id, description: @store.id
     end
   end
 
@@ -17,7 +21,7 @@ class FlyerReaderTool < RubyLLM::Tool
     prices = []
     scanned_products.each do |p|
       product = Product.find_or_create_by(
-        name: p[:name],
+        name: :name,
         description: :description,
         keyword: :keyword
       )
@@ -28,10 +32,10 @@ class FlyerReaderTool < RubyLLM::Tool
       end
 
       price = Price.new(
-        # store_id: store_id,
+        store_id: @store.id,
         product_id: product.id,
-        price_without_tax: :price_without_tax,
-        price_with_tax: :price_with_tax
+        price_without_tax: :price_without_tax.to_s,
+        price_with_tax: :price_with_tax.to_s
       )
       if price.save
         { success: true, price_id: price.id, product_id: product.id }
@@ -40,7 +44,6 @@ class FlyerReaderTool < RubyLLM::Tool
         { success: false, errors: price.errors.full_messages }
       end
     end
-    # return array of prices
     prices
   end
 end
