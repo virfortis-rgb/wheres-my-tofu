90e52757e042d907215599c85a095bdbfb4c9095
Damien Joubert <damienusvirfortis@gmail.com>
2026-03-09 17:30:17 +0900

    update tool params

--- app/tools/flyer_reader_tool.rb
+++ app/tools/flyer_reader_tool.rb
@@ -6,17 +6,17 @@ class FlyerReaderTool < RubyLLM::Tool
   end
 
   params do
-    object :scanned_product do
-      string :name, description: "商品名"
-      string :description, description: "商品説明文"
-      string :keyword, description: "One keyword to define this product"
-      number :price_without_tax, description: "税抜き価格"
-      number :price_with_tax, description: "税込価格"
-      # string :store_id, description: @store.id
+    array :scanned_products do
+      object do
+        string :name, description: "商品名"
+        string :description, description: "商品説明文"
+        string :keyword, description: "One keyword to define this product"
+        number :price_without_tax, description: "税抜き価格"
+        number :price_with_tax, description: "税込価格"
+      end
     end
   end
 
-  # Todo update arguments, receive array? of products => array gives a NoMethodError
   def execute(scanned_product:)
     prices = []
     # p = p.with_indifferent_access
