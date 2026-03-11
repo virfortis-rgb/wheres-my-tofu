361a22a0572b782b4659232ecdf1e80f7ef5a06e
Damien Joubert <damienusvirfortis@gmail.com>
2026-03-11 17:16:11 +0900

    update schema

--- app/schemas/flyer_schema.rb
+++ app/schemas/flyer_schema.rb
@@ -1,11 +1,11 @@
 class FlyerSchema < RubyLLM::Schema
   array :scanned_products do
     object do
-      string :name, description: "商品名"
-      string :description, description: "商品説明文"
-      string :keyword, description: "One keyword to define this product"
-      string :price_without_tax, description: "The price without tax"
-      string :price_with_tax, description: "The price with tax (税込)"
+      string :name, description: "Product name"
+      string :description, description: "Additional description"
+      string :keyword, description: "category keyword"
+      number :price_without_tax, description: "Price excluding tac"
+      number :price_with_tax, description: "Price includinfg tax"
     end
   end
 end
