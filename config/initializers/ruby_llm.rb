6cf2f10b62a81ee256b22277fcb7728c4cf74669
Damien Joubert <damienusvirfortis@gmail.com>
2026-03-11 17:02:28 +0900

    gemini

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -4,6 +4,6 @@ RubyLLM.configure do |config|
   config.openai_api_base = "https://api.groq.com/openai/v1"
   config.gemini_api_key = ENV["GEMINI_API_KEY"]
   config.gemini_api_base = 'https://generativelanguage.googleapis.com/v1'
-  config.default_model = 'qwen/qwen2.5-vl-32b-instruct:free'
+  # config.default_model = 'qwen/qwen2.5-vl-32b-instruct:free'
   # config.default_model = 'gpt-5'
 end
