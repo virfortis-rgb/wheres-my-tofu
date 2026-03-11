9a4a5a42be66f47a370cba93e299c8c6f6249d52
Damien Joubert <damienusvirfortis@gmail.com>
2026-03-11 21:24:40 +0900

    small ui updates

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,8 @@ module WheresMyTofu
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Osaka"
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.active_job.queue_adapter = :solid_queue
